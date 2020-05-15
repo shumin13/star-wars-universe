5228ecbec01804edb941ec078b4217c237050df1
shumin13 <ms.huang.shumin@gmail.com>
2020-05-15 22:02:40 +0800

    Add favicon and logo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducers from './redux/reducers';
