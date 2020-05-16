8a522f64930b00c9081a7a8d55d95189c096bd27
shumin13 <ms.huang.shumin@gmail.com>
2020-05-16 23:49:35 +0800

    Create constants

---
+++ src/common/consts.js
@@ -0,0 +1,50 @@
+export const PROFILE = {
+  birth_year: 'Birth Year',
+  eye_color: 'Eye Color',
+  gender: 'Gender',
+  hair_color: 'Hair Color',
+  height: 'Height',
+  mass: 'Mass',
+  skin_color: 'Skin Color',
+};
+
+export const PLANET = {
+  name: 'Name',
+  diameter: 'Diameter',
+  rotation_period: 'Rotation Period',
+  orbital_period: 'Orbital Period',
+  gravity: 'Gravity',
+  population: 'Population',
+  climate: 'Climate',
+  terrain: 'Terrain',
+  surface_water: 'Surface Water',
+};
+
+export const SPECIES = {
+  name: 'Name',
+  classification: 'Classification',
+  designation: 'Designation',
+  average_height: 'Average Height',
+  average_lifespan: 'Average Lifespan',
+  eye_colors: 'Eye Colors',
+  hair_colors: 'Hair Colors',
+  skin_colors: 'Skin Colors',
+  language: ' Language',
+};
+
+export const FILM = {
+  title: 'Title',
+  episode_id: 'Episode',
+};
+
+export const STARSHIP = {
+  name: 'Name',
+  model: 'Model',
+  starship_class: 'Class',
+};
+
+export const VEHICLE = {
+  name: 'Name',
+  model: 'Model',
+  vehicle_class: 'Class',
+};
